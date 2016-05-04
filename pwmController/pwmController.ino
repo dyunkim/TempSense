47b101a840ce4015c510aa8bb74d72f55599c585
Eileen Guo <eguo@smu.edu>
2016-05-03 23:19:33 -0500

    adding stuff

--- pwmController/pwmController.ino
+++ pwmController/pwmController.ino
@@ -1,5 +1,5 @@
 #include <Servo.h>
-#include <cmath>
+//#include <cmath>
 Servo servo;
 const static int MED_VALUE = 95;
 const static int MIN_VALUE = 60;
@@ -7,28 +7,54 @@ const static int MAX_VALUE = 130;
 const static int temperaturePin = 0;
 const static int motorPin = 9;
 const static float MAX_ERROR_MARGIN = 2.00;
+int pwmVal;
 
 void setup() {
   //  pinMode(motorPin, OUTPUT);
   Serial.begin(9600);
   servo.attach(motorPin);
   servo.write(MED_VALUE);
+  pwmVal = 95;
 }
 
 void loop() {
-  
+//  if (Serial.available())
+//  {
+//    int speed = Serial.parseInt();
+//    Serial.println(speed);
+//    changeTemperature(speed);
+//  }
+//
+//  
+//  readTemperature();
+//  //changeTemperature();
+//  delay(1000);
+
+//  if (Serial.available())
+//  {
+//    int speed = Serial.parseInt();
+//    if (speed >= 60 && speed <= 130)
+//    {
+//      //      analogWrite(motorPin, speed);
+//      Serial.println(speed);
+//      servo.write(speed);
+//    }
+//  }
+  readTemperature();
+  delay(1000);
 }
 
 
 void changeTemperature(int temperature) {
   float error = temperature - readTemperature();
-  while(abs(error) <= MAX_ERROR_MARGIN) {
+  while(abs(error) >= MAX_ERROR_MARGIN) {
     if (error > 0) {
-      changePwm(
+      pwmVal += 1;
+      changePwm(pwmVal);
     } else {
-      
+      pwmVal -= 1;
+      changePwm(pwmVal);
     }
-    
     delay(1000);
     error = temperature - readTemperature();
   }
@@ -40,26 +66,31 @@ float mapTempDiff(float error) {
 
 bool changePwm(int value) {
   if(value >= MIN_VALUE && value <= MAX_VALUE) {
-    Serial.println("Given value: " + value + "\n");  //debugging
+    Serial.print("Given value: ");
+    Serial.print(value);
+    Serial.print("\n");  //debugging
     servo.write(value);
+    Serial.println(pwmVal);
     return true;
   }
   else {
     Serial.print("value out of range\n");
+    pwmVal = 95;
     return false;
   }
 }
 
 
-void readTemperature() {
+int readTemperature() {
   float voltage, degreesC, degreesF;
 
   voltage = getVoltage(temperaturePin);
   degreesC = (voltage - .5) * 100.0;
   degreesF = degreesC * (9.0/5.00) + 32.0;
 
-  Serial.print("Degrees F: " + "\n");
+  Serial.print("Degrees F: ");
   Serial.println(degreesF);
+  return degreesF;
 }
 
 float getVoltage(int pin) {
